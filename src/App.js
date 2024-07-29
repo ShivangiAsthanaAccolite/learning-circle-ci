e8d54b68e2c057adbcef581e76f0bfc87fac8943
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-07-29 18:34:49 +0530

    updating App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
-      <h1>Hello World</h1>
+      </header>
     </div>
   );
 }
