7204c720bd973035bbb17353ec085d8d96ed3aef
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-07-30 17:04:12 +0530

    updated files

--- src/components/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-import Home from "./Home";
-import React from "react";
-
-test("renders Home component", () => {
-  render(<Home />);
-  expect(screen.getByText("Home Page")).toBeInTheDocument();
-});
--- src/components/Main.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-import Main from "./Main";
-import React from "react";
-
-test("renders Main component", () => {
-  render(<Main />);
-  expect(screen.getByText("Main Page")).toBeInTheDocument();
-});
